639999f1bc60a6fc786a4640cd98ffb275f94a89
truongnh3 <truongnh3@vng.com.vn>
2022-12-16 00:29:31 +0700

    init project

---
+++ src/main/java/ptithcm/multicast/MulticastApplication.java
@@ -0,0 +1,13 @@
+package ptithcm.multicast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulticastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ptithcm/multicast/MulticastApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.multicast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
